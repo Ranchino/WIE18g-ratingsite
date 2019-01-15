a9b76506f1b40824a1442b22032328b12a867d50
Ranj Bahadin <ranjbahadin@live.se>
2019-01-15 14:47:26 +0100

    making multidimensional array

--- arraylist.php
+++ arraylist.php
@@ -1,7 +1,16 @@
 <?php
-    $topfilm = ["5. Black Panther" => "T'Challa, heir to the hidden but advanced kingdom of Wakanda, must step forward to lead his people into a new future and must confront a challenger from his country's past.",
+    /* $topfilm = ["5. Black Panther" => "T'Challa, heir to the hidden but advanced kingdom of Wakanda, must step forward to lead his people into a new future and must confront a challenger from his country's past.",
     "2. A Star is Born" => "A musician helps a young singer find fame, even as age and alcoholism send his own career into a downward spiral.",
     "4. Roma" => "A year in the life of a middle-class family's maid in Mexico City in the early 1970s.",
     "3. BlacKKKlans" => "Ron Stallworth, an African American police officer from Colorado Springs, CO, successfully manages to infiltrate the local Ku Klux Klan branch with the help of a Jewish surrogate who eventually becomes its leader.",
-    "1. Annihilation" => "A biologist signs up for a dangerous, secret expedition into a mysterious zone where the laws of nature don't apply."];
+    "1. Annihilation" => "A biologist signs up for a dangerous, secret expedition into a mysterious zone where the laws of nature don't apply."]; */
+
+    $topfilm = [
+        array("name" => "Black Panther", "description" => "T'Challa, heir to the hidden but advanced kingdom of Wakanda, must step forward to lead his people into a new future and must confront a challenger from his country's past"),
+        array("name" => "A Star is Born", "description" => "A musician helps a young singer find fame, even as age and alcoholism send his own career into a downward spiral"),
+        array("name" => "Roma", "description" => "A year in the life of a middle-class family's maid in Mexico City in the early 1970s"),
+        array("name" => "BlacKKKlans", "description" => "Ron Stallworth, an African American police officer from Colorado Springs, CO, successfully manages to infiltrate the local Ku Klux Klan branch with the help of a Jewish surrogate who eventually becomes its leader"),
+        array("name" => "Annihilation", "description" => "A biologist signs up for a dangerous, secret expedition into a mysterious zone where the laws of nature don't apply"),
+
+    ];
 ?>
